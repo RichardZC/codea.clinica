504bd8dc8e957528b70296d0a11ecfb08889c3e8
Richard Zapata <r_zapatac@hotmail.com>
2018-09-26 01:01:47 -0500

    modelo

--- ScriptBD/Tabla/Usuario.sql
+++ ScriptBD/Tabla/Usuario.sql
@@ -2,7 +2,7 @@
 (
 	UsuarioId INT NOT NULL PRIMARY KEY identity(1,1),
 	PersonaId int references Persona(PersonaId) NOT NULL,
-	Usuario VARCHAR(50) not null,
+	[Nombre] VARCHAR(50) not null,
 	Clave VARCHAR(50) NOT NULL,
 	[Activo] bit default 1 NOT NULL, 
     [IndReseteo] BIT default 1 NOT NULL
