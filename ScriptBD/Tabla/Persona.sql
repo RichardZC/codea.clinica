bdcfa80b45e5232e39bea9f07ba5f8841b4ce369
Richard Zapata <r_zapatac@hotmail.com>
2018-09-26 00:30:57 -0500

    script BD inicio

---
+++ ScriptBD/Tabla/Persona.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE Persona
+(
+	PersonaId INT NOT NULL PRIMARY KEY identity(1,1),
+	Apellidos VARCHAR(255) NOT NULL,
+	Nombres VARCHAR(255) NOT NULL,
+	TipoDoc CHAR(3) default 'DNI',
+	NumDoc VARCHAR(11),
+	Celular varchar(10) ,
+	Correo varchar(100) ,
+	Sexo CHAR(1) DEFAULT 'F',
+	Nacimiento date,
+	Direccion varchar(512)
+)
---
+++ ScriptBD/Tabla/Usuario.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [Usuario]
+(
+	UsuarioId INT NOT NULL PRIMARY KEY identity(1,1),
+	PersonaId int references Persona(PersonaId) NOT NULL,
+	Usuario VARCHAR(50) not null,
+	Clave VARCHAR(50) NOT NULL,
+	[Activo] bit default 1 NOT NULL, 
+    [IndReseteo] BIT default 1 NOT NULL
+)
